87fba4bd28a10bc398f52573d8fadea8097a077c
tboyd4 <tboyd1898@gmail.com>
2019-12-22 22:27:06 -0600

    added an array of the 5 questions, represented in five objects within the array

---
+++ assets/JS/question.js
@@ -0,0 +1,29 @@
+// this javascript file is dedicated to the questions that will be accessed in my main.js
+
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "What is the HTML tag under which one can write the JavaScript code?",
+    choices: ["<javascript>", "<scripted>", "<script>", "<js>"],
+    answer: "<script>"
+  },
+  {
+    title: "The external JavaScript file must contain <script> tag. True or False?",
+    choices: ["True", "False"],
+    answer: "True"
+  },
+  {
+    title: "How is the function called in JavaScript?",
+    choices: ["call function", "call function ()", "function ()", "func call ()"],
+    answer: "function ()"
+  },
+  {
+    title: "What is the correct syntax for adding comments in JavaScript?",
+    choices: ["// this is a comment", "<!-- this is a comment", "-* this is a comment", "@#@ this is a comment"],
+    answer: "// this is a comment"
+  }
+];
