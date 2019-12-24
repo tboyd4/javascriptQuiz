77d35c8967ca97f2aa11b5a06b1beb8fd1345b96
tboyd4 <tboyd1898@gmail.com>
2019-12-24 14:28:54 -0600

    used DOM/query selector to grab all the elements on the quiz.html

---
+++ assets/JS/main.js
@@ -0,0 +1,18 @@
+// main code will run here
+
+//grabbing important parts of html through DOM
+
+let getStart = document.querySelector('#button-start-quiz');
+let getChoiceA = document.querySelector('#choice-A');
+let getChoiceB= document.querySelector('#choice-B');
+let getChoiceC = document.querySelector('#choice-C');
+let getChoiceD = document.querySelector('#choice-D');
+
+console.log(getStart);
+console.log(getChoiceA);
+console.log(getChoiceB);
+console.log(getChoiceC);
+console.log(getChoiceD);
+
+
+
