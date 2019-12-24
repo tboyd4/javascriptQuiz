b1c8b5657a1b1191c9b339a7dc8eff78b88863e8
tboyd4 <tboyd1898@gmail.com>
2019-12-24 14:29:11 -0600

    adding questions

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -2,17 +2,17 @@
 
 //grabbing important parts of html through DOM
 
+// selecting all buttons
 let getStart = document.querySelector('#button-start-quiz');
 let getChoiceA = document.querySelector('#choice-A');
 let getChoiceB= document.querySelector('#choice-B');
 let getChoiceC = document.querySelector('#choice-C');
 let getChoiceD = document.querySelector('#choice-D');
 
-console.log(getStart);
-console.log(getChoiceA);
-console.log(getChoiceB);
-console.log(getChoiceC);
-console.log(getChoiceD);
+//selecting question text area
+let getQuestion = document.querySelector('#question-text');
+
+
 
 
 
