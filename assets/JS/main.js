831a20c22d33fcd007b229147ba7077e7d50f190
tboyd4 <tboyd1898@gmail.com>
2019-12-25 21:14:58 -0600

    got quiz texts to reflect 1st question upon hitting start

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -3,7 +3,7 @@
 //grabbing important parts of html through DOM
 
 // selecting all buttons
-let getStart = document.querySelector('#button-start-quiz');
+let getStart = document.querySelector('#btn-start-quiz');
 let getChoiceA = document.querySelector('#choice-A');
 let getChoiceB= document.querySelector('#choice-B');
 let getChoiceC = document.querySelector('#choice-C');
@@ -12,6 +12,23 @@ let getChoiceD = document.querySelector('#choice-D');
 //selecting question text area
 let getQuestion = document.querySelector('#question-text');
 
+
+// The quiz will begin upon the click of the start button
+
+function startQuiz () {
+    console.log('now I am really here');
+    quizText();
+}
+
+// Function to reassign quiz text to reflect question
+
+function quizText () {
+    getQuestion.textContent = questions[0].title;
+    getChoiceA.textContent = questions[0].choices[0];
+    getChoiceB.textContent = questions[0].choices[1];
+    getChoiceC.textContent = questions[0].choices[2];
+    getChoiceD.textContent = questions[0].choices[3];
+}
 
 
 
